0d3b7bd3eb5df9c4d0b7a3e8c0265cc3489bfcc6
Bugaev Kirill <DurdenRD@gmail.com>
2023-06-22 09:39:44 +0300

    вводим исходный массив

--- Program.cs
+++ Program.cs
@@ -1,1 +1,38 @@
-﻿Console.WriteLine("hello world");
+﻿// Console.WriteLine("hello world");
+// int [] array = new int[3];
+// Array(array);
+// Printarray(array);
+// Console.Write($"{array[array.Length-1]}");
+
+
+
+// void Printarray(int [] arr)
+
+// {
+//     for (int i = 0; i < arr.Length-1; i++)
+//     {
+//         Console.Write($"{arr[i]}, ");
+//     }
+    
+
+// }
+
+
+
+
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод исходного массива с клавиатуры
+        Console.Write("Введите элементы массива через пробел: ");
+        string[] inputArray = Console.ReadLine().Split(' ');
+
+        
+
+        // Вывод результата
+        Console.WriteLine("Исходный массив: {0}", string.Join(", ", inputArray));
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ExamCode
 # ExamCode
+1. Ввод исходного массива inputArray с клавиатуры
+2. Создание нового массива outputArray з строк, длина которых меньше или равна 3 
+3. string[inputArray.Length] - Создание результирующего массива необходимого размера
+4. int outputIndex = 0 - Индекс следующего элемента в результирующем массиве
+5. Вывод результат
+- Console.WriteLine("Исходный массив: {0}", string.Join(", ", inputArray));
+-         Console.WriteLine("Новый массив: {0}", string.Join(", ", outputArray, 0, outputIndex));
